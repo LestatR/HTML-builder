d44c1b197942de54cf8c53e02db07286ab385fad
ArtsiomRusak <lestat47@tut.by>
2022-11-07 15:47:52 +0300

    fix: use readStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const stream = new fs.ReadStream('./01-read-file/text.txt', 'utf8');
+let fs = require('fs');
+let stream = new fs.ReadStream('./01-read-file/text.txt', 'utf8');
 stream.on('readable', function() {
-    const data = stream.read();
+    let data = stream.read();
     console.log(data);
 });
 stream.on('done', function() {
