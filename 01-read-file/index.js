dc9300aed78b5064f04139c270611c1a496c9c59
ArtsiomRusak <lestat47@tut.by>
2022-11-07 16:05:12 +0300

    feat: use path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 let fs = require('fs');
-let stream = new fs.ReadStream('./01-read-file/text.txt', 'utf8');
+let path = require('path');
+let stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf8');
 stream.on('readable', function() {
     let data = stream.read();
     console.log(data);
